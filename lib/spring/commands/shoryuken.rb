c6bd3f3ed7f4faaeab9f0aa40a9dcf5387d34ae2
Gabriel Holodak <gthepiper@gmail.com>
2019-04-24 00:45:10 -0400

    Handle configure_server properly

--- lib/spring/commands/shoryuken.rb
+++ lib/spring/commands/shoryuken.rb
@@ -1,20 +1,55 @@
 require "spring/commands/shoryuken/version"
 
-# We have to define Shoryuken::CLI so that Shoryuken.configure_server blocks
-# will run during preloading.
+# In a default Shoryuken setup, Shoryuken.configure_server blocks will run for
+# the server (when invoked as `shoryuken` on the command line), and
+# Shoryuken.configure_client blocks will run otherwise. This won't work with
+# Spring, because the client and server processes are forked from the same
+# preloaded base.
+#
+# As a workaround, we can let configure_client blocks run during preloading
+# (assuming they are defined in an initializer, as recommended). We also delay
+# configure_server blocks until after Spring forks, so they are only run for
+# "server" processes. This also has the benefit of avoiding dreaded errors like
+# "A copy of MyCustomMiddleware has been removed from the module tree but is
+# still active!"
+
+# Make sure it's already loaded
+require "shoryuken/options"
+
 module Shoryuken
-  module CLI; end
+  class Options
+    class << self
+      def configure_server
+        ::Spring::Commands::Shoryken.configure_server { yield self }
+      end
+    end
+  end
 end
 
 module Spring
   module Commands
     class Shoryuken
+      @@configure_server_callbacks = []
+
+      def self.configure_server(&blk)
+        @@configure_server_callbacks << blk
+      end
+
       def env(*_)
         "development"
       end
 
+      def gem_name
+        "shoryuken"
+      end
+
       def exec_name
         "shoryuken"
+      end
+
+      def call
+        @@configure_server_callbacks.each(&:call)
+        load Gem.bin_path(gem_name, exec_name)
       end
     end
 
